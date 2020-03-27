d1bde4db5d6bc3b93b0dbca523e971f9cff35491
arvita <aqwuarvitha@gmail.com>
2020-03-27 16:07:52 +0700

    commit

---
+++ app/src/main/java/com/arvita/crudvolley/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.arvita.crudvolley.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.1.7/FrameworkWeb/cimif/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.1.7/FrameworkWeb/cimif/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.1.7/FrameworkWeb/cimif/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.1.7/FrameworkWeb/cimif/Mahasiswa/ApiUpdate";
+}
